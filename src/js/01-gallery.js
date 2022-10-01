540f507fe47152d0f239c697d51b7560e23fc7f6
Oleksandr Pavlov <sashapavlov20@gmail.com>
2022-10-01 14:07:50 +0300

    made gallery with npm and simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,22 @@
-// Add imports above this line
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
-console.log(SimpleLightbox);
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionClass: 'center',
+});
