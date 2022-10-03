01c1226ea373a6f9d47732f195be5aba0ff29890
Oleksandr Pavlov <sashapavlov20@gmail.com>
2022-10-03 14:19:45 +0300

    made 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+playerSetup();
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds);
+  }, 1000)
+);
+
+function playerSetup() {
+  const currentTime = localStorage.getItem(STORAGE_KEY);
+
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  } else player.setCurrentTime(0);
+}
